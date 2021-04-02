774050246d82bf3b8f5519a4fea3549773e0bd25
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-02 14:29:19 +0900

    prototype実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,24 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      @prototype = Prototype.new
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :concept, :catch_copy, :image).merge(user_id: current_user.id)
+  end
+
 end
